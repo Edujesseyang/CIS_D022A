ca77f06d56b6a8d0c2b17a1a396deca715bafb9c
Edujesseyang <edujesseyang@gmail.com>
2024-01-09 16:55:01 -0500

    lab A added

---
+++ Lab_A/lab_A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// LAB A
+// 1.
+int main()
+{
+    int a = 50;
+    int b = 100;
+    int total = a + b;
+    cout << "Total of 50 and 100 is " << total << "." << endl;
+    return 0;
+}
+
+// Optional bonus
+/*
+
+int main()
+{
+    int a, b;
+    cout << "Enter your first number: ";
+    cin >> a;
+
+    cout << "Enter your second number: ";
+    cin >> b;
+
+    int total = a + b;
+    cout << "Total is :" << total << "." << endl;
+    return 0;
+}
+
+*/
