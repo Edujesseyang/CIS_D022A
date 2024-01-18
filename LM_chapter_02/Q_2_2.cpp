4a20f513479cb4f8777b335da518ea04a6721aae
Edujesseyang <edujesseyang@gmail.com>
2024-01-17 19:24:35 -0800

    12

--- LM_chapter_02/Q_2_2.cpp
+++ LM_chapter_02/Q_2_2.cpp
@@ -8,8 +8,8 @@ const double PI = 3.14;
 const double RADIUS = 5.4;
 int main()
 {
-    int area;                        // definition of area of circle
-    int circumference;               // definition of circumference
+    float area;                      // definition of area of circle
+    float circumference;             // definition of circumference
     circumference = 2 * PI * RADIUS; // computes circumference
     area = RADIUS * RADIUS * PI;     // computes area
 
