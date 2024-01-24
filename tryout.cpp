1fe340b990c8ffb45063f4a31bb377c3acda87a8
Edujesseyang <edujesseyang@gmail.com>
2024-01-23 17:10:03 -0800

    a

--- tryout.cpp
+++ tryout.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 int main()
-{
-    string str4;
-    char stopper;
+  {
 
-    cout << "Enter the context for string 4: ";
-    getline(cin, str4);
-    cout << "Enter the char you want stop the storing to string 4: ";
-    cin >> stopper;
-
-    cout << "str4: everything before '" << stopper << "': " << endl;
-    cout << str4 << endl;
+    int units = 5;
+    float mass = 5.33;
+    double weight = M_PI;
+    weight = mass * units;
+    cout << units << endl;
+    cout << mass << endl;
+    cout << weight << endl;
     return 0;
-}
+  }
