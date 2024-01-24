621175ce032f1304e2bb4dab388aaff4b199dc99
Edujesseyang <edujesseyang@gmail.com>
2024-01-23 21:39:05 -0800

    new_update :: speed enter game

--- speed_enter_game/speed_enter_game.cpp
+++ speed_enter_game/speed_enter_game.cpp
@@ -1,48 +1,61 @@
 #include <iostream>
 #include <ctime>
 #include <cmath>
+#include <chrono>
 
 using namespace std;
 
 long get_rand_num();
 long get_user_input();
+long long get_time_ms();
 
 int main()
 {
-
-    cout << "You have 5 sec to enter the target num." << endl;
-    cout << "Are you ready? ";
-    cin.get();
-
-    long target_num = pow(get_rand_num(), 2);
-    cout << "The target number is: " << endl;
-    cout << target_num << endl;
-    cout << "You have 5 sec to enter the target num." << endl;
-
-    cout << "Ready GO!" << endl;
-
-    long time_1 = time(nullptr);
-    long user_input = get_user_input();
-    long time_2 = time(nullptr);
-
-    if (user_input == target_num)
+    char keep_play = 'y';
+    while (keep_play == 'y' || keep_play == 'Y')
     {
-        cout << "Good job. " << endl;
-        if (time_2 - time_1 >= 5)
+        cout << "Game rule:" << endl;
+        cout << "You have 5 sec to enter the target num." << endl;
+        cout << "Are you ready? (y/n)";
+        cin >> keep_play;
+        if (keep_play == 'n' || keep_play == 'N')
         {
-            cout << "But you lose, because you are too slow, ";
+            break;
         }
-        cout << "You spend " << time_2 - time_1 << " s." << endl;
-        if (time_2 - time_1 < 5)
+
+        long target_num = pow(get_rand_num(), 2);
+        cout << "The target number is: " << endl;
+        cout << target_num << endl;
+        cout << "You have 5 sec to enter the target num." << endl;
+
+        cout << "Ready GO!" << endl;
+
+        long long time_1 = get_time_ms();
+        long user_input = get_user_input();
+        long long time_2 = get_time_ms();
+        float use_time = (static_cast<float>(time_2 - time_1)) / 1000;
+
+        if (user_input == target_num)
         {
-            cout << "You are fast as fuck! ";
+            cout << "Good job. " << endl;
+            if (time_2 - time_1 >= 5000)
+            {
+                cout << "But you lose, because you are too slow, ";
+            }
+            cout << "You spend " << use_time << " s." << endl;
+            if (time_2 - time_1 < 5000)
+            {
+                cout << "You are fast as fuck! " << endl;
+                cout << endl;
+            }
+        }
+        else
+        {
+            cout << "Yor entered a wrong number, You lose!" << endl;
+            cout << endl;
         }
     }
-    else
-    {
-        cout << "Yor entered a wrong number, You lose!" << endl;
-    }
-
+    cout << endl;
     cin.get();
     return 0;
 }
@@ -61,4 +74,11 @@ long get_user_input()
     cin >> input_num;
     cin.get();
     return input_num;
+}
+
+long long get_time_ms()
+{
+    auto now = std::chrono::system_clock::now();
+    auto duration = now.time_since_epoch();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
 }
