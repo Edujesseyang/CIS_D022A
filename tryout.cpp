4b0d20dbf37973721eb1b15a0b9b132f40dd3e07
Edujesseyang <edujesseyang@gmail.com>
2024-02-14 12:54:25 -0800

    update

--- tryout.cpp
+++ tryout.cpp
@@ -3,18 +3,8 @@
 int static_var(int input = 100)
 {
 	static int num = input;
-	num += 1;
+	num += input;
 	return num;
-}
-
-int add_up(int x, int y = 0, int z = 0)
-{
-	return x + y + z;
-}
-
-void print_add(int &var)
-{
-	std::cout << var << '\n';
 }
 
 int main(int argc, char *argv[])
@@ -26,17 +16,6 @@ int main(int argc, char *argv[])
 	std::cout << num << '\n';
 	std::cout << num2 << '\n';
 	std::cout << num3 << '\n';
-
-	int sum = add_up(12, 34, 54);
-	int sum1 = add_up(12, 32);
-	int sum1 = add_up(12, 32);
-	int sum1 = add_up(12, 32);
-	std::cout << sum << '\n';
-	std::cout << sum1 << '\n';
-
-	int my_num = 123;
-	print_add(my_num);
-	std::cout << &my_num << '\n'
 
 	return 0;
 }
