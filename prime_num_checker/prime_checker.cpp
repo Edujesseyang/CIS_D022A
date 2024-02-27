8b7a386d0d7e923ee76d99f6c27bd468f9129ba6
Edujesseyang <edujesseyang@gmail.com>
2024-02-27 14:00:44 -0800

    ll

--- prime_num_checker/prime_checker.cpp
+++ prime_num_checker/prime_checker.cpp
@@ -4,29 +4,40 @@ bool is_prime(int num);
 
 int main()
 {
-    std::cout << "Enter the number that you want to check if it is a prime number: \n";
-    int num;
-    std::cin >> num;
-    bool result = is_prime(num);
+    for (size_t i = 1, j = 0; i < 10000; i++)
+    {
+        if (is_prime(i) == true)
+        {
+            std::cout << i << '\t';
+            j++;
+            if (j % 10 == 0)
+            {
+                std::cout << '\n';
+            }
+        }
+
+    }
     return 0;
 }
 
 bool is_prime(int num)
 {
     int factor = 2;
-    while(factor < num)
+    if (num == 2 || num == 1)
     {
-        if(num%factor == 0)
+
+        return true;
+    }
+    while (factor < num)
+    {
+        if (num % factor == 0)
         {
-            std::cout << "It is not a prime number, it can divide " << factor << ".\n";
             return false;
         }
-        if(factor == num -1)
+        if (factor == num - 1)
         {
-            std::cout << "It is a prime number.\n";
             return true;
         }
         factor++;
     }
 }
-
