79f38e5a78337b0d6e3c7c8b562fa6e35357165c
Edujesseyang <edujesseyang@gmail.com>
2024-03-05 15:35:23 -0800

    add

--- Lab_G/Lab_G.cpp
+++ Lab_G/Lab_G.cpp
@@ -1,23 +1,10 @@
-/*
-Rock, Paper, Scissors Game
-Write a program that lets the user play the game of Rock, Paper, Scissors against the computer. The program should work as follows.
-1. When the program begins, a random number in the range of 1 through 3 is generated. If the number is 1, then the computer has chosen rock. If the number is 2, then the computer has chosen paper. If the number is 3, then the computer has chosen scissors. (Don’t display the computer’s choice yet.)
-2. The user enters his or her choice of “rock”, “paper”, or “scissors” at the keyboard. (You can use a menu if you prefer.)
-3. The computer’s choice is displayed.
-4. A winner is selected according to the following rules:
-• If one player chooses rock and the other player chooses scissors, then rock wins.
-(The rock smashes the scissors.)
-• If one player chooses scissors and the other player chooses paper, then scissors
-wins. (Scissors cuts paper.)
-• If one player chooses paper and the other player chooses rock, then paper wins.
-(Paper wraps rock.)
-• If both players make the same choice, the game results in a tie.
-*/
-
 // Name: Jesse Yang
-// program usage:
-// Playing will be asked choose one from scissor, rock, or paper.
-// The computer will choose also. The program will display who is the winner.
+// ****************************************************************************************************************
+// *    program usage:                                                                                            *
+// *            User will be asked choose one from scissor, rock, or paper.                                       *
+// *            The computer will generate a random number to represent one of these choices.                     *
+// *            Then, the program will display who is the winner, and ask if user wants to play again.            *
+// ****************************************************************************************************************
 
 #include <iostream>
 #include <string>
@@ -26,13 +13,33 @@ wins. (Scissors cuts paper.)
 
 std::string get_comp();
 std::string get_user();
-std::string check_winner(string comp, string user);
-// void display_winning(string winner);
+void display_winning(std::string comp, std::string user);
 
 int main()
 {
-    std::cout << "Welcome to Rock Scissor Paper game, "
+    std::cout << "Welcome to Rock Scissor Paper game,\nIn this game, you will play against the computer.\nAre you ready to play?(y/n)\n";
+    char ready;
 
+    while (ready != 'y' || ready != 'Y' || ready != 'n' || ready != 'N')
+    {        
+        std::cout << "Please enter correct character, 'y' for yes, 'n' for no to exit.\n";
+        std::cin >> ready;
+        std::cin.get();
+        while (ready == 'y' || ready == 'Y')
+        {
+            std::string comp_choice = get_comp();
+            std::string user_choice = get_user();
+            display_winning(comp_choice, user_choice);
+            std::cout << "Do you want play again?\n";
+            std::cin >> ready;
+        }
+        if(ready == 'n'||ready == 'N')
+        {
+            break;
+        }
+    }
+    std::cout << "Thank you for using.\n";
+    std::cin.get();
     return 0;
 }
 
@@ -40,18 +47,20 @@ std::string get_comp()
 {
     srand(time(nullptr));
     int comp_num = rand() % 3;
+    std::string result;
     if (comp_num == 0)
     {
-        return "rock";
+        result = "rock";
     }
     else if (comp_num == 1)
     {
-        return "paper";
+        result = "paper";
     }
     else
     {
-        return "scissors";
+        result = "scissors";
     }
+    return result;
 }
 
 std::string get_user()
@@ -61,6 +70,7 @@ std::string get_user()
     {
         std::cout << "Enter your choose, 1 for rock, 2 for paper, 3 for scissors:\n";
         std::cin >> user_input;
+        std::cin.get();
     }
     if (user_input == 1)
     {
@@ -76,41 +86,50 @@ std::string get_user()
     }
 }
 
-std::string check_winner(string comp, string user)
+void display_winning(std::string comp, std::string user)
 {
     std::string winner = "";
     if (comp == "rock")
     {
-        if (user = "paper")
+        if (user == "paper")
         {
-            winner = "User";
+            winner = "you";
         }
         else if (user == "scissors")
         {
             winner = "Computer";
         }
     }
-    else if (comp = "paper")
+    else if (comp == "paper")
     {
         if (user == "scissors")
         {
-            winner = "User";
+            winner = "you";
         }
-        else if (user = "rock")
+        else if (user == "rock")
         {
             winner = "Computer";
         }
     }
-    else if (comp = "scissors")
+    else if (comp == "scissors")
     {
         if (user == "rock")
         {
-            winner = "User";
+            winner = "you";
         }
-        else if(user == "paper")
+        else if (user == "paper")
         {
             winner = "Computer";
         }
     }
-    return winner;
+    std::cout << "You had choose " << user << ".\n";
+    std::cout << "Computer had choose " << comp << ".\n";
+    if (winner != "")
+    {
+        std::cout << "The winner for this round is " << winner << ".\n";
+    }
+    else
+    {
+        std::cout << "This round is tie.\n";
+    }
 }
