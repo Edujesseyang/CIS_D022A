218f47462b3c68e8496cb1af290e0d6313fc5e5a
Jesse <edujesseyang@gmail.com>
2024-03-08 21:02:41 -0800

    newa

--- tryout.cpp
+++ tryout.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
 
-int num, num1;
-int sum = func2(num, num1);
-
-
 int main()
 {
-	int a = func2();
+	std::cout << "Hello World!";
 	
 	return 0;
 }
 
-int func2(num, num1)
-{
-	std::cout << "work";
-}
