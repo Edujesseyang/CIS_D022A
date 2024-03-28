5cb516096e3de9625a9eb0ba9531ef3b51c3ce10
Edujesseyang <edujesseyang@gmail.com>
2024-03-27 18:40:32 -0700

    New change

--- try.cpp
+++ try.cpp
@@ -6,5 +6,6 @@ int main()
     std::cout << "New change.\n";
     std::cout << "another try.\n";
     std::cout << "Just for practice.\n";
+    std::cout << "This is new practice.\n";
     return 0;
 }
