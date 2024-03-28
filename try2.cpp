d07a75a9f3142f46dd7bf5c1aef4028cd08e6dad
Edujesseyang <edujesseyang@gmail.com>
2024-03-28 16:21:20 -0700

    add

--- try2.cpp
+++ try2.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "here is new try\n";
-    std::cout << "Added something.\n";
+int main() {
+    const int a = 15;  // Here, var is const.
+    const int *ptr_a = &a;  // ptr is pointing a const, but the ptr is not a const.
+    
+    int b = 12;  // Here, var is not a const.
+    int const *ptr_b = &b;  // But, ptr is a const, pointing a non-const var.
+    
+
+    std::cout << ptr_a << '\n';
+    std::cout << *ptr_a << '\n';
+
+    std::cout << ptr_b << '\n';
+    std::cout << *ptr_b << '\n';
+
+
+
+
     return 0;
 }
