49dfa3abe629e0bb5c22b4f4978305717321401d
Edujesseyang <edujesseyang@gmail.com>
2024-03-28 16:50:13 -0700

    a

--- try2.cpp
+++ try2.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 
 int main() {
-    const int a = 15;  // Here, var is const.
-    const int *ptr_a = &a;  // ptr is pointing a const, but the ptr is not a const.
-    
-    int b = 12;  // Here, var is not a const.
-    int const *ptr_b = &b;  // But, ptr is a const, pointing a non-const var.
-    
-
-    std::cout << ptr_a << '\n';
-    std::cout << *ptr_a << '\n';
-
-    std::cout << ptr_b << '\n';
-    std::cout << *ptr_b << '\n';
+	int arr[3] = {1, 2, 3};
+	
+	std::cout << "arr= " << arr << std::endl;  // Here, cout the memory location of the first element in the array.
+	std::cout << "arr[0]= " << &arr[0] << std::endl;  // It will be same as cout arr itself.
+	
+	int *ptr_arr = arr;  // Defined a int* to pointing the mem loc of arr.
+ 	
+	for (int num : arr)
+        std::cout << num << ' ' ;  // If we print all elements, the if will be "100 2 3".
 
 
 
