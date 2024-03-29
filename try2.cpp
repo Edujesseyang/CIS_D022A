b335076ec00e0e5ba55ab3e08566e92ea0bb5420
Edujesseyang <edujesseyang@gmail.com>
2024-03-28 23:39:27 -0700

    a

--- try2.cpp
+++ try2.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 
-int main() {
-	int arr[3] = {1, 2, 3};
-	
-	std::cout << "arr= " << arr << std::endl;  // Here, cout the memory location of the first element in the array.
-	std::cout << "arr[0]= " << &arr[0] << std::endl;  // It will be same as cout arr itself.
-	
-	int *ptr_arr = arr;  // Defined a int* to pointing the mem loc of arr.
- 	
-	for (int num : arr)
-        std::cout << num << ' ' ;  // If we print all elements, the if will be "100 2 3".
-
-    std::cout << '\n';
-    std::cout << "arr[1]= " << *(ptr_arr + 1) << std::endl;
-    *(ptr_arr + 1) = 14;
-    std::cout << "arr[1]= " << *(ptr_arr + 1) << std::endl;
-
+int my_rec(int n);
+int main()
+{
+    std::cout << my_rec(-2) << '\n';
+    std::cout << my_rec(-1) << '\n';
+    std::cout << my_rec(0) << '\n';
+    std::cout << my_rec(3) << '\n';
 
     return 0;
+}
+int my_rec(int n)
+{
+    if (n < 0)
+        return -1;
+    return my_rec(n + 1);
 }
